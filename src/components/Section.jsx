e3b0a2d98f84cba49b266ff0d8ad97bc3d20f6d2
muradgm <59296531+muradgm@users.noreply.github.com>
2023-01-14 19:03:59 +0100

    remove the padding-y for section to the solve the issue that occurs when the show report modal is open

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Section = ({ children, classes }) => {
   return (
-    <div className={`p-10 space-y-2 ${classes ? classes : ""}`}>{children}</div>
+    <div className={`p-10 space-y-0 ${classes ? classes : ""}`}>{children}</div>
   );
 };
 
